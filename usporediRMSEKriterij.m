dbfc3d07a1d5fa6bb1ea884d046ce893d764e98e
TeaBakic <tea.bakic16@gmail.com>
2026-09-18 10:54:08 +0200

    Update usporediRMSEKriterij.m
    Updated comments for clarity and corrected naming conventions.

--- usporediRMSEKriterij.m
+++ usporediRMSEKriterij.m
@@ -1,17 +1,9 @@
-%% usporediRMSEKriterij.m  (ISPRAVLJENA VERZIJA)
+%% usporediRMSEKriterij.m  
 %
-% Uspoređuje redoslijed hvatanja predmeta prema kriteriju "najbolji RMSE
-% prvi". Budući da coordinator.Parts NE sprema RMSE vrijednost (koristi
-% se samo privremeno tijekom detekcije, pa se odbacuje), ova skripta
-% RMSE ponovno izračunava izravno, istim postupkom kao u
-% prikaziICPPoravnanje.m: uzima pripadajući segment iz
-% coordinator.PointCloudSegments (prema polju .index) i poravnava ga s
-% odgovarajućim CAD modelom (prema polju .type).
-%
-% PRETPOSTAVKA: 'coordinator' već postoji, Build Environment i
-% Detect Parts su već pokrenuti.
+% Uspoređuje redoslijed hvatanja predmeta prema kriteriju "najbolji RMSE prvi". 
+% PRETPOSTAVKA: 'coordinator' već postoji, Build Environment i Detect Parts su već pokrenuti.
 
-%% Učitaj CAD modele (isto kao u izvornom kodu)
+%% Učitavanje CAD modela (isto kao u izvornom kodu)
 bottle = stlread('exampleHelperBottlePoints.stl');
 can = stlread('exampleHelperCanPoints.stl');
 pcBottle = pointCloud(bottle.Points);
@@ -35,8 +27,6 @@ rmse1 = izracunajRMSE(part1, pcBottle, pcCan, coordinator);
 rmse2 = izracunajRMSE(part2, pcBottle, pcCan, coordinator);
 
 nazivi = ["Boca", "Limenka"];
-% dodijeli citljiv naziv prema type polju (1 = boca, 2 = limenka), ne
-% prema pretpostavci da je Parts{1} uvijek boca
 naziv1 = nazivi(part1.type);
 naziv2 = nazivi(part2.type);
 
@@ -55,7 +45,6 @@ end
 fprintf('\nPrema RMSE kriteriju, preporučeni redoslijed: %s\n', preporuceniRedoslijed);
 fprintf('Razlika u RMSE vrijednostima: %.4f\n', razlika);
 
-%% Spremi rezultat kao tablicu
 resultsTable = table([naziv1; naziv2], [rmse1; rmse2], ...
     'VariableNames', {'Predmet', 'RMSE'});
 disp(resultsTable);
