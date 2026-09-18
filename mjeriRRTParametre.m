5a4d7d6f3433e3c3dc76e000f5232dc3620f5230
TeaBakic <tea.bakic16@gmail.com>
2026-09-18 10:41:20 +0200

    Update mjeriRRTParametre.m

--- mjeriRRTParametre.m
+++ mjeriRRTParametre.m
@@ -5,30 +5,6 @@
 % vrijeme planiranja, duljinu puta, broj tocaka puta i postotak
 % uspjesnosti planiranja.
 %
-% PRETPOSTAVKA: 'coordinator' objekt vec postoji u MATLAB workspaceu
-% (dakle, vec si pokrenula pripremu i Build Environment korak, pa
-% coordinator.Robot i coordinator.World postoje i sadrze stvarni
-% model robota i stvarne prepreke iz tvoje scene).
-%
-% Skripta NE treba aktivnu ROS/Gazebo vezu za samo planiranje (planer
-% radi iskljucivo nad MATLAB modelom), pa je mozes pokrenuti i nakon
-% sto si zavrsila skeniranje, cak i bez daljnje potrebe za Gazebom.
-%
-% Rezultat: tri tablice (u Command Windowu) + tri .csv datoteke
-% spremne za izravno kopiranje u Word tablicu (Insert -> Table ->
-% Insert from file, ili samo copy-paste iz Excela nakon otvaranja csv-a).
-
-%% Postavke koje prilagodi svom scenariju
-robot = coordinator.Robot;
-world = coordinator.World;
-
-% Pocetna konfiguracija - npr. Home pozicija
-startConfig = coordinator.CurrentRobotJConfig;
-
-% Ciljna konfiguracija - npr. konfiguracija za hvatanje prvog predmeta.
-% Ako GraspPose jos nije izracunata, izracunaj je prije pokretanja ove
-% skripte (exampleCommandComputeGraspPoseROSGazeboScene(coordinator)),
-% ili ovdje rucno postavi neku drugu validnu ciljnu pozu.
 ik = inverseKinematics('RigidBodyTree', robot);
 rng(1);
 goalConfig = ik(coordinator.RobotEndEffector, coordinator.GraspPose, ...
@@ -60,7 +36,6 @@ function resultsTable = testParameter(robot, world, startConfig, goalConfig, ...
             planner.EnableConnectHeuristic = true; % polazna (default) vrijednost
             planner.SkippedSelfCollisions = "adjacent";
 
-            % Prepisi bas onaj parametar koji se u ovoj petlji testira
             switch paramName
                 case "MaxConnectionDistance"
                     planner.MaxConnectionDistance = v;
@@ -84,9 +59,6 @@ function resultsTable = testParameter(robot, world, startConfig, goalConfig, ...
                 npoints(end+1) = size(path, 1); %#ok<AGROW>
                 successes = successes + 1;
             catch
-                % Planiranje nije uspjelo (nije pronaden put) - ne
-                % ubraja se u prosjek vremena/duljine, ali utjece na
-                % postotak uspjesnosti ispod.
             end
         end
 
@@ -133,4 +105,3 @@ writetable(resultsECH, 'rezultati_EnableConnectHeuristic.csv');
 
 %% Gotovo
 disp('Sve tri tablice spremljene su kao .csv datoteke u trenutnom MATLAB direktoriju.');
-disp('Otvori ih u Excelu, kopiraj sadrzaj, i zalijepi kao tablicu u Word (Paste Special -> HTML ili kao obicnu tablicu).');
